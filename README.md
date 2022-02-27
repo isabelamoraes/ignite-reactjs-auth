e293b263aa1221a70cfdc85f167fcb0db007f79f
Isabela Moraes <isabelagmoraes@hotmail.com>
2022-02-27 18:13:28 -0300

    Upload to git

---
+++ README.md
@@ -0,0 +1,46 @@
+<h1 align="center">
+  Auth
+</h1>
+
+<h4 align="center">
+  Auth flow example
+</h4>
+
+<p align="center">
+  <a href="#features">Features</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#getting-started">Getting Started</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#reference">Reference</a>
+</p>
+
+## Features
+
+This project was developed with the following technologies:
+
+- **[Next.js](https://nextjs.org/)** - A framework for creating React applications.
+- **[Node JS](https://nodejs.org/)** - A runtime enviroment for JavaScript applications.
+
+## Getting Started
+
+To clone and run this application, place from your command line:
+
+```bash
+# Clone this repository
+$ git clone https://github.com/isabelamoraes/ignite-reacjs-auth.git
+
+# Go into the repository
+$ cd ignite-reacjs-auth
+
+# Go into the backend or frontend repository
+$ cd <backend | fronted>
+
+# Install dependencies
+$ yarn
+
+# Run the node application
+$ yarn dev
+
+```
+
+## Reference
+
+This application was developed during the classes of the Chapter IV of the ReactJS track on Ignite, training promoted by Rocketseat.
---
+++ frontend/services/api.ts
@@ -0,0 +1,107 @@
+import axios, { AxiosError } from "axios";
+import { parseCookies, setCookie } from "nookies";
+import { signOut } from "../context/AuthContext";
+import { AuthTokenError } from "./errors/AuthTokenError";
+
+let isRefreshing = false;
+let failedRequestsQueue = [];
+
+export function setupAPIClient(context = undefined) {
+  let cookies = parseCookies(context);
+
+  const api = axios.create({
+    baseURL: "http://localhost:3333",
+    headers: {
+      Authorization: `Bearer ${cookies["nextauth.token"]}`,
+    },
+  });
+
+  api.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error: AxiosError) => {
+      if (error.response.status === 401) {
+        if (error.response.data?.code === "token.expired") {
+          cookies = parseCookies(context);
+
+          const { "nextauth.refreshToken": refreshToken } = cookies;
+
+          const originalConfig = error.config;
+
+          if (!isRefreshing) {
+            isRefreshing = true;
+
+            api
+              .post("/refresh", {
+                refreshToken,
+              })
+              .then((response) => {
+                const { token } = response.data;
+
+                setCookie(context, "nextauth.token", token, {
+                  maxAge: 60 * 60 * 24 * 30, //30 dias
+                  path: "/",
+                });
+
+                setCookie(
+                  context,
+                  "nextauth.refreshToken",
+                  response.data.refreshToken,
+                  {
+                    maxAge: 60 * 60 * 24 * 30, //30 dias
+                    path: "/",
+                  }
+                );
+
+                api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+                failedRequestsQueue.forEach((request) =>
+                  request.onSuccess(token)
+                );
+
+                failedRequestsQueue = [];
+              })
+              .catch((error) => {
+                failedRequestsQueue.forEach((request) =>
+                  request.onFailure(error)
+                );
+
+                failedRequestsQueue = [];
+
+                if (process.browser) {
+                  signOut();
+                } else {
+                  return Promise.reject(new AuthTokenError());
+                }
+              })
+              .finally(() => {
+                isRefreshing = false;
+              });
+          }
+
+          return new Promise((resolve, reject) => {
+            failedRequestsQueue.push({
+              onSuccess: (token: string) => {
+                originalConfig.headers["Authorization"] = `Bearer ${token}`;
+
+                resolve(api(originalConfig));
+              },
+              onFailure: (error: AxiosError) => {
+                reject(error);
+              },
+            });
+          });
+        } else {
+          if (process.browser) {
+            signOut();
+          }
+        }
+      }
+
+      return Promise.reject(new AuthTokenError());
+    }
+  );
+
+  return api;
+}
